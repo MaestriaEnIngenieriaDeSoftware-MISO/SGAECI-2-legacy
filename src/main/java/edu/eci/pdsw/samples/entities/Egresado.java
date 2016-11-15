1809c4eb387fe6ad019261be325fd51938fad23f
cristianpinzon97 <cristian.pinzon@mail.escuelaing.edu.co>
2016-11-14 23:30:31 -0500

    Update Egresado.java

--- src/main/java/edu/eci/pdsw/samples/entities/Egresado.java
+++ src/main/java/edu/eci/pdsw/samples/entities/Egresado.java
@@ -5,6 +5,8 @@
  */
 package edu.eci.pdsw.samples.entities;
 
+import java.util.Date;
+
 /**
  *
  * @author 2106796
@@ -12,14 +14,16 @@ package edu.eci.pdsw.samples.entities;
 public class Egresado extends Persona{
     private String semestreGrado,correoPersonal,labora,cargo;
     private Egresado_Empresa emp;
+    private Date fechaGraduacion;
     
-    public Egresado(int documentoID, long telefono1, long telefono2,String tipoDocumentoID, String nombre, String direccion, String correo, String genero, String semestreGrado, String correoPersonal, String cargo, String labora,Egresado_Empresa egresadoEmpresa) {
+    public Egresado(int documentoID, long telefono1, long telefono2,String tipoDocumentoID, String nombre, String direccion, String correo, String genero, String semestreGrado, String correoPersonal, String cargo, String labora,Egresado_Empresa egresadoEmpresa, Date graduacion) {
         super(documentoID,telefono1,telefono2,tipoDocumentoID,nombre,direccion,correo,genero);
         this.semestreGrado = semestreGrado;
         this.correoPersonal = correoPersonal;
         this.cargo=cargo;
         this.emp=egresadoEmpresa;
         this.labora=labora;
+        this.fechaGraduacion=graduacion;
     }
 
     public Egresado() {
@@ -63,6 +67,14 @@ public class Egresado extends Persona{
 
     public void setEmp(Egresado_Empresa emp) {
         this.emp = emp;
+    }
+
+    public Date getFechaGraduacion() {
+        return fechaGraduacion;
+    }
+
+    public void setFechaGraduacion(Date fechaGraduacion) {
+        this.fechaGraduacion = fechaGraduacion;
     }
 
     
