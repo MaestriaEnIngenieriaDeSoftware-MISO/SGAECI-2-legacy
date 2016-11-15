61b247cf2ab115caf1ed82a333c27b5567a736ba
Carlos Alberto Ramirez Otero <carlos.ramirez-ot@mail.escuelaing.edu.co>
2016-11-15 00:13:46 -0500

    Arreglo del trigger en el pom

---
+++ triggers.sql
@@ -0,0 +1,29 @@
+--TRIGGERS
+DELIMITER \\
+CREATE OR REPLACE TRIGGER nuevo_estado_afiliacion
+AFTER UPDATE
+   ON Solicitud_Afiliacion FOR EACH ROW
+BEGIN
+    DECLARE
+        tempDocumnentoID int;
+    SELECT DocumentoID FROM Egresado where NEW.DocumentoID=Egresado.DocumentoID INTO tempDocumnentoID; 		
+		
+        IF(tempDocumnentoID IS NOT NULL)THEN
+            IF((NEW.Estado_Solicitud="ACEPTADA")) THEN 
+                IF ((SELECT DATEDIFF(SYSDATE(),Fecha_Graduacion) FROM Egresado WHERE NEW.DocumentoID=Egresado.DocumentoID)<=180) THEN 
+                    INSERT INTO Estado_afiliacion (DocumentoID,Fecha_Inicio,Fecha_Fin,Estado)
+                    VALUES
+                    (NEW.DocumentoID,SYSDATE(),DATE_ADD(SYSDATE(),INTERVAL 180 DAY),"ACTIV0");
+                ELSE
+                    INSERT INTO Estado_afiliacion (DocumentoID,Fecha_Inicio,Fecha_Fin,Estado)
+                    VALUES
+                    (NEW.DocumentoID,SYSDATE(),DATE_ADD(SYSDATE(),INTERVAL 30 DAY),"INACTIVO - PENDIENTE PAGO CUOTA");
+                END IF;
+            END IF;
+        ELSE 
+            INSERT INTO Estado_afiliacion (DocumentoID,Fecha_Inicio,Fecha_Fin,Estado)
+            VALUES
+            (NEW.DocumentoID,SYSDATE(),DATE_ADD(SYSDATE(),INTERVAL 180 DAY),"ACTIVO");
+        END IF;
+END//
+DELIMITER;
