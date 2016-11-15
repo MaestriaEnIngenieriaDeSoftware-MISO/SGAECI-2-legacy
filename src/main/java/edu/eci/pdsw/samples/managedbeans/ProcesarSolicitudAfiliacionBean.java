1f8839f87f158e164a830dced65f5ab9e8735e93
cristianpinzon97 <cristian.pinzon@mail.escuelaing.edu.co>
2016-11-14 23:16:44 -0500

    Update ProcesarSolicitudAfiliacionBean.java

--- src/main/java/edu/eci/pdsw/samples/managedbeans/ProcesarSolicitudAfiliacionBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/ProcesarSolicitudAfiliacionBean.java
@@ -45,17 +45,16 @@ public class ProcesarSolicitudAfiliacionBean implements Serializable{
     ServiciosSAGECI SAGECI=ServiciosSAGECI.getInstance();
     SolicitudAfiliacion solicitudSelection;
     String Comentario;
-    AsignacionUser_password user;
-    String usuarioAs= user.getUsuario();
-    String passAs=user.getPassword();
+    //AsignacionUser_password user;
+    //String usuarioAs= user.getUsuario();
+    //String passAs=user.getPassword();
     EmailSender sender = new SimpleEmailSender(new EmailConfiguration());
     Email email = null;
     final String from = "5d8dd682c0-c92f3e@inbox.mailtrap.io";
     final String subjectAprobado = "Solicitud de Ingreso AECI: Aprobada";
     final String messageRechazado = "Su solicitud ha sido Rechazada por lo siguiente: "+Comentario;
-    final String usuarioasignado = "Su usuario de ingreso asignado es: "+usuarioAs; // se informa al afiliado, el usuario asignado por el sistema
-    final String passwordasignado = "Su contraseña de ingreso asignado es: "+passAs; // se informa al afiliado, la contraseña  asignado por el sistema
-
+    //final String usuarioasignado = "Su usuario de ingreso asignado es: "+usuarioAs;
+    //final String passwordasignado = "Su contraseña de ingreso asignado es: "+passAs;
     public ProcesarSolicitudAfiliacionBean() {
         
     }
